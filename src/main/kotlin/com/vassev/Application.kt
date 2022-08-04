f463c42ea93d58f2200e340ed5ea0dea7074b208
PiotrV <piotrv1001@gmail.com>
2022-08-04 18:22:32 +0200

    first commit

---
+++ src/main/kotlin/com/vassev/Application.kt
@@ -0,0 +1,15 @@
+package com.vassev
+
+import io.ktor.server.application.*
+import com.vassev.plugins.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+    configureRouting()
+}
